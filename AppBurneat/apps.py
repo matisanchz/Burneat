411f876b381dc923ee582a69e8edde8cadd1f99e
Matías Sánchez Abrego <matiassanchezabrego@gmail.com>
2022-04-06 11:36:33 -0300

    Primer commit

---
+++ AppBurneat/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppburneatConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppBurneat'
--- ProjectBurneat/asgi.py
+++ ProjectBurneat/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for AppBurneat project.
+ASGI config for ProjectBurneat project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppBurneat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectBurneat.settings')
 
 application = get_asgi_application()
