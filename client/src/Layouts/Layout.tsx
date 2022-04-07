971aa16614730bdc1e8b0f19f257435b195ac3cd
Emanuel Constancio <emanuelconstancio.dev@gmail.com>
2022-04-07 12:45:39 -0300

    Layout component

--- client/src/Layouts/Layout.tsx
+++ client/src/Layouts/Layout.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
- 
+import { Navbar, Footer } from './index';
 
 const Layout = ({children}) => {
   return (
-    <>
-      {/* <Header />
-        {children}
-      <Footer /> */}
+    <>  
+        <Navbar/> 
+            {children}
+        <Footer />
     </>
   )
 }
